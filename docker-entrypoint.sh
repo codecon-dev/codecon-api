52e78e957a7a77f4508336f159029ee4349aeb7b
Marcelo Kopmann <marcelokopmann@gmail.com>
2024-10-22 15:11:47 -0300

    feat: add docker entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Run database migrations
+npm run migration:run
+
+# Run database seed (if we have one in the future)
+# npm run seed
+
+# Start the application
+exec npm run start:prod
